f8771f0b0481a0a8c277d000612b77105aff6abc
Gaurav Kumar <aazzroy@gmail.com>
2024-02-03 02:30:40 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,175 @@
+/* =================================
+------------------------------------
+	Gaurav - Resume
+	Version: 1.0
+ ------------------------------------ 
+ ====================================*/
+
+
+
+'use strict';
+
+
+$(window).on('load', function() { 
+	/*------------------
+		Preloder
+	--------------------*/
+	$(".loader").fadeOut(); 
+	$("#preloder").delay(400).fadeOut("slow");
+
+});
+
+
+(function($) {
+
+	/*------------------
+		Background set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+
+	$('.review-slider').owlCarousel({
+		loop: true,
+		nav: false,
+		dots: true,
+		items: 1,
+		autoplay: true
+	});
+
+
+
+	$('.progress-bar-style').each(function() {
+		var progress = $(this).data("progress");
+		var prog_width = progress + '%';
+		if (progress <= 100) {
+			$(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
+		}
+		else {
+			$(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
+		}
+	});
+
+
+	$('.lan-prog').each(function() {
+		var progress = $(this).data("lanprogesss");
+		var ele      = '<span></span>';
+		var ele_fade = '<span class="fade-ele"></span>';
+		
+		for (var i = 1; i <= 5; i++) {
+			if(i <= progress){
+				$(this).append(ele);
+			} else {
+				$(this).append(ele_fade);
+			}
+		}
+	});
+
+
+	/*------------------
+		Popup
+	--------------------*/
+	$('.Certificate-item .port-pic').magnificPopup({
+		type: 'image',
+		mainClass: 'img-popup-wrap',
+		removalDelay: 500,
+	});
+
+
+
+
+if($().circleProgress){
+
+	//Set progress circle 1
+	$("#progress1").circleProgress({
+		value: 0.75,
+		size: 175,
+		thickness: 2,
+		fill: "#40424a",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+	//Set progress circle 2
+	$("#progress2").circleProgress({
+		value: 0.83,
+		size: 175,
+		thickness: 2,
+		fill: "#40424a",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+
+	//Set progress circle white
+	$("#progress3").circleProgress({
+		value: 0.95,
+		size: 175,
+		thickness: 2,
+		fill: "#ffffff",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+
+	//Set progress circle white
+	$("#progress4").circleProgress({
+		value: 0.85,
+		size: 175,
+		thickness: 2,
+		fill: "#ffffff",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+
+	//Set progress circle skyblue
+	$("#progress5").circleProgress({
+		value: 0.75,
+		size: 175,
+		thickness: 2,
+		fill: "#009fff",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+
+
+	document.getElementById("contactForm").addEventListener("submit", function(event) {
+		event.preventDefault(); // Prevent the default form submission
+	  
+		// Collect form data
+		var formData = new FormData(this);
+	  
+		// Send form data to your custom endpoint
+		var endpointURL = "https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjUwNTZkMDYzZTA0M2Q1MjZjNTUzNDUxMzQi_pc";
+		fetch(endpointURL, {
+		  method: "POST",
+		  body: formData
+		})
+		.then(function(response) {
+		  if (response.ok) {
+			// Form submission succeeded
+			console.log("Form submitted successfully");
+			// You can perform additional actions here, such as displaying a success message
+		  } else {
+			// Form submission failed
+			console.error("Form submission failed");
+			// You can handle the error or display an error message to the user
+		  }
+		})
+		.catch(function(error) {
+		  console.error("An error occurred during form submission:", error);
+		  // Handle the error or display an error message to the user
+		});
+	  });
+	  
+
+
+
+	
+	//Set progress circle skyblue
+	$("#progress6").circleProgress({
+		value: 0.83,
+		size: 175,
+		thickness: 2,
+		fill: "#009fff",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+}
+
+
+})(jQuery);
+
